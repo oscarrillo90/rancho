30c04783c9085b66c041b444e507925262e2ff37
oscar carrillo <oscarrillo008@outlook.com>
2017-07-12 00:27:52 -0500

    all templates display data

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 # main_app/urls.py
 from django.conf.urls import url
-from views import index, menu, about, gallery, home, appetizers
+from views import index, menu, about, gallery, home, appetizers, soupSalad, texmex, entrees, mexican, kids, lunch
 
 
 urlpatterns = [
@@ -10,6 +10,11 @@ urlpatterns = [
     url(r'^about/$', about, name="about"),
     url(r'^gallery/$', gallery, name="gallery"),
     url(r'^appetizers/$', appetizers, name="appetizers"),
-
+    url(r'^soupSalad/$', soupSalad, name="soupSalad"),
+    url(r'^texmex/$', texmex, name="texmex"),
+    url(r'^entrees/$', entrees, name="entrees"),
+    url(r'^mexican/$', mexican, name="mexican"),
+    url(r'^kids/$', kids, name="kids"),
+    url(r'^lunch/$', lunch, name="lunch"),
 
 ]
--- main/views.py
+++ main/views.py
@@ -29,5 +29,46 @@ def appetizers(request):
     priceOptions = PriceOption.objects.all()
     print(appetizers)
     print("Price Options: " , priceOptions)
-
     return render(request, 'appetizers.html', {'appetizers': appetizers, 'priceOptions': priceOptions})
+
+def soupSalad(request):
+    soupSalad = Dish.objects.filter(menu_category='soup/salad')
+    priceOptions = PriceOption.objects.all()
+    print(soupSalad)
+    print("Price Options: " , priceOptions)
+    return render(request, 'soup-salad.html', {'soupSalad': soupSalad, 'priceOptions': priceOptions})
+
+def texmex(request):
+    texmex = Dish.objects.filter(menu_category='texmex')
+    priceOptions = PriceOption.objects.all()
+    print(texmex)
+    print("Price Options: " , priceOptions)
+    return render(request, 'texmex.html', {'texmex': texmex, 'priceOptions': priceOptions})
+
+def entrees(request):
+    entrees = Dish.objects.filter(menu_category='entrees')
+    priceOptions = PriceOption.objects.all()
+    print(entrees)
+    print("Price Options: " , priceOptions)
+    return render(request, 'entrees.html', {'entrees': entrees, 'priceOptions': priceOptions})
+
+def mexican(request):
+    mexican = Dish.objects.filter(menu_category='mexican')
+    priceOptions = PriceOption.objects.all()
+    print(mexican)
+    print("Price Options: " , priceOptions)
+    return render(request, 'mexican.html', {'mexican': mexican, 'priceOptions': priceOptions})
+
+def kids(request):
+    kids = Dish.objects.filter(menu_category='kids')
+    priceOptions = PriceOption.objects.all()
+    print(kids)
+    print("Price Options: " , priceOptions)
+    return render(request, 'kids.html', {'kids': kids, 'priceOptions': priceOptions})
+
+def lunch(request):
+    lunch = Dish.objects.filter(menu_category='lunch')
+    priceOptions = PriceOption.objects.all()
+    print(lunch)
+    print("Price Options: " , priceOptions)
+    return render(request, 'lunch.html', {'lunch': lunch, 'priceOptions': priceOptions})
